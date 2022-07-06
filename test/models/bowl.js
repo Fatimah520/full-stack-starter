49cb12ed4027b606cc56dbb5224c3a0bc6278c56
Fatimah Abdolcader <fatimah.abdolcader@gmail.com>
2022-07-05 19:46:53 -0700

    fixed unit test errors

--- test/models/bowl.js
+++ test/models/bowl.js
@@ -5,7 +5,7 @@ const models = require('../../models');
 
 describe('models.bowls', () => {
   beforeEach(async () => {
-    await helper.loadFixtures([]);
+    await helper.loadFixtures(['bowls']);
   });
 
   it('creates a new Item record', async () => {
@@ -20,5 +20,9 @@ describe('models.bowls', () => {
     item = await models.bowls.findByPk(item.id);
     assert.deepStrictEqual(item.Name, 'Test Title');
     assert.deepStrictEqual(item.short, 'This is longer test Text.');
+  });
+  it('returns all the bowls', async () => {
+    const results = await models.bowls.findAll();
+    assert.deepStrictEqual(results.length, 2);
   });
 });
